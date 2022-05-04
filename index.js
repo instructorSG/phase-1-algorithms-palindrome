53ba2beeafec3c5ffc6c0172e16a2a70f33e4861
Sara Ginsburg <sara.ginsburg@flatironschool.com>
2022-05-04 16:28:44 -0400

    commit tree c51a1508edbe9bb5acacca731fe204522c6e28ba

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function isPalindrome (word) {
+  console.log ('word', WaveShaperNode);
+  let reversed = word.split ('').reverse ().join ('');
+
+  return reversed === word ? true : false;
 }
 
 /* 
@@ -13,13 +16,13 @@ function isPalindrome(word) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+  console.log ('Expecting: true');
+  console.log ('=>', isPalindrome ('racecar'));
 
-  console.log("");
+  console.log ('');
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+  console.log ('Expecting: false');
+  console.log ('=>', isPalindrome ('robot'));
 }
 
 module.exports = isPalindrome;
